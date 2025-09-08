52584447c55893ac59dc139a44f2db3e105242ec
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2025-09-08 10:34:39 +0200

    first commit

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('index', () => {
+  it('test', () => {
+    expect(true).toBe(true);
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export const kp = 1;
+
+
+export default kp;
